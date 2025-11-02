cb214214af7811ce4f00f481e0e13e16e92e18aa
USER <USER@192.168.1.3>
2025-11-02 20:02:09 +0700

    tugasPekan5

---
+++ 2025_prakalpro_A_2511532003/src/Pekan5/TugasPekan5.java
@@ -0,0 +1,31 @@
+package Pekan5;
+
+public class TugasPekan5 {
+	public static void main (String [] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <=(-2 * i + 10) ; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("<");
+			for (int j = 1; j < i; j++) {
+				System.out.print("....");
+			}
+			System.out.print(">");
+			System.out.println();
+			}
+		for (int i = 1; i <= 4; i++) {
+			for ( int j = 1; j <= (2 * i); j++ ) {
+				System.out.print(" ");
+			}
+			System.out.print("<");
+			for ( int j = 1; j <= (-1 * i + 4); j++) {
+				System.out.print("....");
+			}
+			System.out.print(">");
+			System.out.println();
+		}
+		
+			}
+			
+		}
+
