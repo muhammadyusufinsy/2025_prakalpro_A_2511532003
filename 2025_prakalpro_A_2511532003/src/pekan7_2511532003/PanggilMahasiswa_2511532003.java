45ddf6b14ac9423505680e9b98af61fb2f482686
USER <USER@10.44.8.236>
2025-11-10 14:43:03 +0700

    pekan 7 prakalpro A 2511532003

---
+++ 2025_prakalpro_A_2511532003/src/pekan7_2511532003/PanggilMahasiswa_2511532003.java
@@ -0,0 +1,14 @@
+package pekan7_2511532003;
+
+public class PanggilMahasiswa_2511532003 {
+	public static void main (String [] args ) {
+		Mahasiswa_2511532003 a = new Mahasiswa_2511532003();
+		a.setNim(251153200);
+		a.setNama("Yusuf");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
+
